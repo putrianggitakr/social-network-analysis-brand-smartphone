d72e43f993cdc4d6d31cfdd71b23a8c670a1876e
Putri Anggita Kristanti <88384409+putrianggitakr@users.noreply.github.com>
2022-04-17 21:27:54 +0700

    Add files via upload

---
+++ crawlingsamsung.R
@@ -0,0 +1,104 @@
+#import library
+library(rtweet)
+library(dplyr)
+library(tidyr)
+library(NLP)
+library(tm)
+library(stringr)
+library(caret)
+#library(katadasaR)
+library(tau)
+library(parallel)
+library(twitteR)
+library(RCurl)
+library(ggplot2)
+library(SnowballC)
+#library(Rstem)
+library(RColorBrewer)
+library(wordcloud)
+library(tokenizers)
+
+keyword <- "Samsung"
+jumlahtweet <- 1000
+bahasa <- "id"
+
+token <- create_token(
+  app = "SNA Brand HP",
+  consumer_key = "KEM8sASVTh57NuPpi0cbEpBee",
+  consumer_secret = "eC9PUlqSKFjXC9QMRADq2to7lKnBcCjalVCJpP6Iy7SgWZGxMr",
+  access_token = "298884110-GSfJ1Xb3DxK9jedJ7M9g6gYt7W5JmAbWhayoNeK6",
+  access_secret = "DbQ6OvCsZ2iRsFYVIFqt77mdaNEN3Xgt9amRcnfXtk7s8")
+
+crawling1 <- search_tweets(keyword,
+                           n = jumlahtweet,
+                           since="2022-04-01",
+                           until="2022-04-11",
+                           lang = bahasa,
+                           retryonratelimit = FALSE)
+
+View(crawling1)
+
+#save sebagai csv
+write_as_csv(crawling1, "data_mentah_samsung.csv",
+             prepend_ids = TRUE, na="", fileEncoding = "UTF-8")
+
+#mengimpor dataset dengan memilih langsung file menurut saya
+crawling2 <-read.csv(file.choose(), header = TRUE)
+
+
+#hanya melihat data text saja
+crawling2<-crawling2[5]
+View(crawling2)
+
+#mengubah file ke dalam corpus
+corpusdata <- Corpus(VectorSource(crawling2$'text'))
+View(corpusdata)
+
+#mengubah semua huruf kapital menjadi huruf kecil
+data_casefolding <- tm_map(corpusdata,content_transformer(tolower))
+
+#menghapus url pada dokumen
+removeURL <- function(x)gsub("http[^[:space:]]*","",x)
+data_URL <- tm_map(data_casefolding, content_transformer(removeURL))
+
+#menghapus mention pada dokumen
+remove.mention <- function(x)gsub("@\\S+","",x)
+data_mention <- tm_map(data_URL,remove.mention)
+
+#menghapus hastag
+remove.hashtag <- function(x)gsub("#\\S+","",x)
+data_hashtag <- tm_map(data_mention, remove.hashtag)
+
+#menghapus NL
+remove.NL <- function(x)gsub("\n","",x)
+data_NL <- tm_map(data_hashtag,remove.NL)
+
+#menghapus koma
+replace.comma <- function(x)gsub(",","",x)
+data_comma <- tm_map(data_NL,replace.comma)
+#untuk melihat data yang sudah diedit
+inspect (data_comma[1:100])
+
+#menghapus RT
+remove.RT <-function(x)gsub("RT","",x)
+data_RT <- tm_map(data_comma,remove.RT)
+
+#Menghapus titik 2
+remove.titik2 <- function(x)gsub(":","",x)
+data_titik2 <- tm_map(data_RT,remove.titik2)
+
+#menghapus titik koma
+remove.titikkomma <- function(x)gsub(";","",x)
+data_titikkomma <- tm_map(data_titik2,remove.titikkomma)
+
+#menghapus tanda baca
+data_punctuation <- tm_map (data_titikkomma,content_transformer(removePunctuation))
+
+#menghapus spasi berlebih
+data_whitespace <- tm_map(data_punctuation, stripWhitespace)
+
+#menyimpan data bersih
+databersih<-data.frame(text=unlist(sapply(data_whitespace,'[')),stringsAsFactors = F)
+write.csv(databersih,"data_bersih_samsung.csv")
+
+
